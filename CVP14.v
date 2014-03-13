5eee5dd6c1d2d1d267108b154a5fdef2887560e2
David <smartydude010@hotmail.com>
2014-03-12 21:30:12 -0500

    Update CVP14.v
    Outline

--- CVP14.v
+++ CVP14.v
@@ -2,7 +2,27 @@ module CVP14(output [15:0] Addr, output RD, output WR, output V, output U,
     output [15:0] DataOut, input Reset, input Clk1, input Clk2, input [15:0] DataIn);
 
 //Parameters for opcodes
-parameter VAAD = 4'b0000, VDOT = 4'b0001, SMUL = 4'b0010, SST = 4'b0011, VLD = 4'b0100,
-          VST = 4'b0101,  SLL = 4'b0110,  SLH = 4'b0111,  J = 4'b1000,   NOP = 4'b1111;
+parameter vaad = 4'b0000, vdot = 4'b0001, smul = 4'b0010, sst = 4'b0011, vld = 4'b0100,
+          vst = 4'b0101,  sll = 4'b0110,  slh = 4'b0111,  j = 4'b1000,   nop = 4'b1111;
+          
+//Decode the data to branch to the proper operation
+always @(DataIn) begin
+  case (DataIn[15:12])
+    vaad:
+    vdot:
+    smul:
+    sst:
+    vld:
+    vst:
+    sll:
+    slh:
+    j:
+    nop:
+  endcase
+end//End Opcode decode
 
+//Overflow/underflow condition
+always @(U, V) begin
+  
+end//End overflow/underflow
 endmodule
