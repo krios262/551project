6c10f59f0258accf68037bae40d7ca3e100b0b3e
David <smartydude010@hotmail.com>
2014-03-12 21:12:41 -0500

    Update CVP14.v
    Added opcode params

--- CVP14.v
+++ CVP14.v
@@ -1,4 +1,8 @@
 module CVP14(output [15:0] Addr, output RD, output WR, output V, output U,
     output [15:0] DataOut, input Reset, input Clk1, input Clk2, input [15:0] DataIn);
 
+//Parameters for opcodes
+parameter VAAD = 4'b0000, VDOT = 4'b0001, SMUL = 4'b0010, SST = 4'b0011, VLD = 4'b0100,
+          VST = 4'b0101,  SLL = 4'b0110,  SLH = 4'b0111,  J = 4'b1000,   NOP = 4'b1111;
+
 endmodule
