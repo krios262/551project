23815bca3a5e97c3974368da047aeac18338a34c
Evan Thompson <evan.g.thompson@gmail.com>
2014-03-24 14:00:30 -0500

    vReg parallel functionality works

--- vReg.v
+++ vReg.v
@@ -26,21 +26,59 @@ module vReg(output reg [255:0] DataOut_p, output reg [15:0] DataOut_s,
   always@(posedge Clk2) begin
     address <= Addr;
   end
-  /*
+  
   always@(readp, address) begin
-    if (readp)
-      DataOut_p = vector[address];
+    if (readp) begin
+      DataOut_p = {vector[address][15], vector[address][14], 
+                   vector[address][13], vector[address][12], vector[address][11], 
+                   vector[address][10], vector[address][9], vector[address][8], 
+                   vector[address][7], vector[address][6], vector[address][5], 
+                   vector[address][4], vector[address][3], vector[address][2], 
+                   vector[address][1], vector[address][0]}; 
+    end
     else
       DataOut_p = DataOut_p;
   end
-
+  
   always@(writep, address) begin
-    if (writep)
-      vector[address] = DataIn_p;
-    else
-      vector[address] = vector[address];
+    if (writep) begin
+      vector[address][0] = DataIn_p[15:0];
+      vector[address][1] = DataIn_p[31:16];
+      vector[address][2] = DataIn_p[47:32];
+      vector[address][3] = DataIn_p[63:48];
+      vector[address][4] = DataIn_p[79:64];
+      vector[address][5] = DataIn_p[95:80];
+      vector[address][6] = DataIn_p[111:96];
+      vector[address][7] = DataIn_p[127:112];
+      vector[address][8] = DataIn_p[143:128];
+      vector[address][9] = DataIn_p[159:144];
+      vector[address][10] = DataIn_p[175:160];
+      vector[address][11] = DataIn_p[191:176];
+      vector[address][12] = DataIn_p[207:192];
+      vector[address][13] = DataIn_p[223:208];
+      vector[address][14] = DataIn_p[239:224];
+      vector[address][15] = DataIn_p[255:240];
+    end
+    else begin
+      vector[address][0] = vector[address][0]; 
+      vector[address][1] = vector[address][1]; 
+      vector[address][2] = vector[address][2]; 
+      vector[address][3] = vector[address][3]; 
+      vector[address][4] = vector[address][4]; 
+      vector[address][5] = vector[address][5]; 
+      vector[address][6] = vector[address][6]; 
+      vector[address][7] = vector[address][7]; 
+      vector[address][8] = vector[address][8]; 
+      vector[address][9] = vector[address][9]; 
+      vector[address][10] = vector[address][10]; 
+      vector[address][11] = vector[address][11]; 
+      vector[address][12] = vector[address][12]; 
+      vector[address][13] = vector[address][13]; 
+      vector[address][14] = vector[address][14]; 
+      vector[address][15] = vector[address][15]; 
+    end
   end
-  */
+  
   always@(select) begin
     if (reads)
       DataOut_s = vector[address][select];
@@ -101,6 +139,24 @@ module t_vReg();
     */
     #160;
     rd = 1'b0;
+    #25;
+    addr = 3'b010;
+    #5;
+    wrp = 1'b1;
+    inp = 256'h123456789abcdef;
+    #10;
+    wrp = 1'b0;
+    rdp = 1'b1;
+    #10;
+    $display("Parallel out: %h", outp);
+    rdp = 1'b0;
+    #25;
+    addr = 3'b001;
+    #5;
+    rdp = 1'b1;
+    #10;
+    $display("Parallel out (should be all x): %h", outp);
+    rdp = 1'b0;
     #20;
     $finish;
   end //initial
