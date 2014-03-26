7d39e5c60ebf8a66ba7c8cb535138a224f40f5c9
Evan Thompson <evan.g.thompson@gmail.com>
2014-03-26 14:24:34 -0500

    Updated CVP14 for 2-output vReg

--- CVP14.v
+++ CVP14.v
@@ -5,9 +5,9 @@ module CVP14(output [15:0] Addr, output RD, output WR, output V,
   parameter vadd = 4'b0000, vdot = 4'b0001, smul = 4'b0010, sst = 4'b0011, vld = 4'b0100,
             vst = 4'b0101,  sll = 4'b0110,  slh = 4'b0111,  j = 4'b1000,   nop = 4'b1111;
 
-  reg [255:0] vOutP, vInP;
-  reg [15:0] sOut, sIn, vOutS, vInS;
-  reg [2:0] sAddr, vAddr;
+  reg [255:0] vOutP, vOutP2, vInP;
+  reg [15:0] sOut, sIn, vOutS, vOutS2, vInS;
+  reg [2:0] sAddr, vAddr, vAddr2;
   reg sRD, sWR, sWR_l, sWR_h, vWR_p, vWR_s, vRD_p, vRD_s;
           
   //Scalar and Vector registers
@@ -15,7 +15,7 @@ module CVP14(output [15:0] Addr, output RD, output WR, output V,
               .RD(sRD), .WR(sWR), .WR_l(sWR_l), .WR_h(sWR_h));
   vReg vector(.DataOut_p(vOutP), .DataOut_s(vOutS), .Addr(vAddr), .Clk1(Clk1), .Clk2(Clk2),
               .DataIn_p(vInP), .DataIn_s(vInS), .WR_p(vWR_p), .RD_p(vRD_p), .WR_s(vWR_s),
-              .RD_s(vRD_s)); 
+              .RD_s(vRD_s), .DataOut2_p(vOutP2), .DataOut2_s(vOutS2), .Addr2(vAddr2)); 
 
   //Decode the data to branch to the proper operation
   always @(DataIn) begin
