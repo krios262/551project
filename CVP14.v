ae0c591e8cb6766d85bdd93e95029a6ff62bc5f1
Evan Thompson <evan.g.thompson@gmail.com>
2014-03-27 16:13:02 -0500

    Incomplete PC implementation

--- CVP14.v
+++ CVP14.v
@@ -1,5 +1,5 @@
-module CVP14(output [15:0] Addr, output RD, output WR, output V,
-    output [15:0] DataOut, input Reset, input Clk1, input Clk2, input [15:0] DataIn);
+module CVP14(output reg [15:0] Addr, output reg RD, output reg WR, output reg V,
+    output reg [15:0] DataOut, input Reset, input Clk1, input Clk2, input [15:0] DataIn);
 
   //Parameters for opcodes
   parameter vadd = 4'b0000, vdot = 4'b0001, smul = 4'b0010, sst = 4'b0011, vld = 4'b0100,
@@ -10,6 +10,10 @@ module CVP14(output [15:0] Addr, output RD, output WR, output V,
   reg [2:0] sAddr, vAddr, vAddr2;
   reg sRD, sWR, sWR_l, sWR_h, vWR_p, vWR_s, vRD_p, vRD_s;
           
+  reg [15:0] PC; //program counter
+  reg [15:0] instruction;
+  reg newPC, getNewInst, readInst;
+
   //Scalar and Vector registers
   sReg scalar(.DataOut(sOut), .Addr(sAddr), .Clk1(Clk1), .Clk2(Clk2), .DataIn(sIn), 
               .RD(sRD), .WR(sWR), .WR_l(sWR_l), .WR_h(sWR_h));
@@ -17,10 +21,56 @@ module CVP14(output [15:0] Addr, output RD, output WR, output V,
               .DataIn_p(vInP), .DataIn_s(vInS), .WR_p(vWR_p), .RD_p(vRD_p), .WR_s(vWR_s),
               .RD_s(vRD_s), .DataOut2_p(vOutP2), .DataOut2_s(vOutS2), .Addr2(vAddr2)); 
 
+  //Operation modules
+
+  always@(posedge Clk1) begin
+    if (newPC) begin
+      Addr <= PC; //Memory reads Addr on clk2, so doing this here avoids violating setup/hold
+      newPC <= 1'b0;
+      readInst <= 1'b1;
+    end
+    else begin
+      Addr <= Addr;
+      newPC <= newPC;
+      readInst <= readInst;
+    end
+  end
+
+  always@(posedge Clk2) begin
+    if (Reset)
+      PC <= 16'h0000;
+    else
+      PC <= PC; //TODO is reset code in the right location?
+
+    if (readInst) begin
+      RD <= 1'b1;
+      readInst <= 1'b0;
+      getNewInst <= 1'b1;
+    end
+    else begin
+      RD <= RD;
+      readInst <= readInst;
+      getNewInst <= getNewInst;
+    end
+
+    if (getNewInst) begin
+      instruction <= DataIn;
+      RD <= 1'b0;
+      getNewInst <= 1'b0;
+    end
+    else begin
+      instruction <= instruction;
+      RD <= RD;
+      getNewInst <= getNewInst;
+    end
+  end
+
   //Decode the data to branch to the proper operation
-  always @(DataIn) begin
-    /*
-    case (DataIn[15:12])
+  always @(instruction) begin
+    
+    getNewInst = 1'b0;
+
+    case (instruction[15:12])
       vadd:
       vdot:
       smul:
@@ -32,7 +82,10 @@ module CVP14(output [15:0] Addr, output RD, output WR, output V,
       j:
       nop:
     endcase
-    */
+
+    PC = PC + 1; //these 2 should probably go in their own always block
+    newPC  = 1'b1; //that activates on module "done" flags
+    
   end//End Opcode decode
 
   //Overflow condition
