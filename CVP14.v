d12bb59161aced2bb298f2ad9e007716d59ba983
Evan Thompson <evan.g.thompson@gmail.com>
2014-03-28 18:07:29 -0500

    Updated CVP14 to work with new sReg and vReg
    Pre-synthesis functional - has not been synthesized yet

--- CVP14.v
+++ CVP14.v
@@ -6,7 +6,7 @@ module CVP14(output reg [15:0] Addr, output reg RD, output reg WR, output reg V,
             vst = 4'b0101,  sll = 4'b0110,  slh = 4'b0111,  j = 4'b1000,   nop = 4'b1111;
 
   //Parameters for states
-  parameter newPC = 3'b000, fetchInst = 3'b001, startEx = 3'b010, executing = 3'b011, 
+  parameter newPC = 3'b000, fetchInst = 3'b001, startEx = 3'b010, executing = 3'b011,
             done = 3'b100, start = 3'b111;
 
   reg  [255:0] vInP;
@@ -15,7 +15,7 @@ module CVP14(output reg [15:0] Addr, output reg RD, output reg WR, output reg V,
   wire [15:0]  vOutS, vOutS2, sOut;
   reg  [2:0]   sAddr, vAddr, vAddr2;
   reg sRD, sWR, sWR_l, sWR_h, vWR_p, vWR_s, vRD_p, vRD_s;
-          
+
   reg [15:0] PC; //program counter
   reg [15:0] instruction;
   reg [15:0] f_Addr;
@@ -23,21 +23,39 @@ module CVP14(output reg [15:0] Addr, output reg RD, output reg WR, output reg V,
   reg [2:0] state, nextState;
 
   //Scalar and Vector registers
-  sReg scalar(.DataOut(sOut), .Addr(sAddr), .Clk1(Clk1), .Clk2(Clk2), .DataIn(sIn), 
+  sReg scalar(.DataOut(sOut), .Addr(sAddr), .Clk1(Clk1), .Clk2(Clk2), .DataIn(sIn),
               .RD(sRD), .WR(sWR), .WR_l(sWR_l), .WR_h(sWR_h));
   vReg vector(.DataOut_p(vOutP), .DataOut_s(vOutS), .Addr(vAddr), .Clk1(Clk1), .Clk2(Clk2),
               .DataIn_p(vInP), .DataIn_s(vInS), .WR_p(vWR_p), .RD_p(vRD_p), .WR_s(vWR_s),
-              .RD_s(vRD_s), .DataOut2_p(vOutP2), .DataOut2_s(vOutS2), .Addr2(vAddr2)); 
+              .RD_s(vRD_s), .DataOut2_p(vOutP2), .DataOut2_s(vOutS2), .Addr2(vAddr2));
 
   //Operation modules
 
   always@(posedge Clk1) begin
-   if (Reset)
-     state <= start;
-   else
-     state <= nextState;
+    if (Reset)
+      state <= start;
+    else
+      state <= nextState;
 
-   Addr <= f_Addr;
+    Addr <= f_Addr;
+    case (instruction[15:12])
+      /*vadd:
+      vdot:
+      smul:
+      sst:
+      vld:
+      vst:*/
+      sll:
+      begin
+        sAddr = instruction[11:9];
+      end
+      slh:
+      begin
+        sAddr = instruction[11:9];
+      end
+      //j:
+      //nop:
+    endcase
   end
 
   always@(posedge Clk2) begin
@@ -46,10 +64,15 @@ module CVP14(output reg [15:0] Addr, output reg RD, output reg WR, output reg V,
 
       start: begin
         RD <= 1'b0;
+        WR <= 1'b0;
+        sWR_l <= 1'b0;
+        sWR <= 1'b0;
+        sWR_h <= 1'b0;
+        sRD <= 1'b0;
       end
 
       newPC: begin
-        RD <= 1'b1;  
+        RD <= 1'b1;
       end
 
       fetchInst: begin
@@ -66,12 +89,14 @@ module CVP14(output reg [15:0] Addr, output reg RD, output reg WR, output reg V,
           sst:
           vld:
           vst:*/
-          sll: 
+          sll:
           begin
+            sIn   <= {sIn[15:8],instruction[7:0]};
             sWR_l <= 1'b1;
           end
           slh:
           begin
+            sIn   <= {instruction[7:0],sIn[7:0]};
             sWR_h <= 1'b1;
           end
           //j:
@@ -93,7 +118,7 @@ module CVP14(output reg [15:0] Addr, output reg RD, output reg WR, output reg V,
   end
 
   always @(state, instruction) begin
-    
+
     case (state)
 
       start: begin
@@ -103,7 +128,7 @@ module CVP14(output reg [15:0] Addr, output reg RD, output reg WR, output reg V,
       end
 
       newPC: begin
-        
+
         nextState = fetchInst;
       end
 
@@ -121,16 +146,12 @@ module CVP14(output reg [15:0] Addr, output reg RD, output reg WR, output reg V,
           sst:
           vld:
           vst:*/
-          sll: 
+          sll:
           begin
-            sAddr = instruction[11:9]; 
-            sIn   = {sIn[15:8],instruction[7:0]}; 
             nextState = done;
           end
           slh:
           begin
-            sAddr = instruction[11:9]; 
-            sIn   = {instruction[7:0],sIn[7:0]}; 
             nextState = done;
           end
           //j:
@@ -140,20 +161,20 @@ module CVP14(output reg [15:0] Addr, output reg RD, output reg WR, output reg V,
       end
 
       executing: begin
-
+        nextState = done; //modify when adding more inst
       end
 
       done: begin
-        PC = PC + 1; 
+        PC = PC + 1;
         f_Addr = PC;
         nextState = newPC;
       end
     endcase
-    
+
   end //combinational block
 
   //Overflow condition
   always @(V) begin
-  
+
   end//End overflow
 endmodule
--- t_CVP14.v
+++ t_CVP14.v
@@ -4,31 +4,46 @@ module t_CVP14();
   wire[15:0] out, in, addr;
   wire r, w, v;
   reg  rst, c1, c2;
-  
+
   DRAM mem(.DataOut(in), .Addr(addr), .DataIn(out),
     .clk1(c1), .clk2(c2), .RD(r), .WR(w));
   CVP14 UUT(.Addr(addr), .RD(r), .WR(w), .V(v),
     .DataOut(out), .Reset(rst), .Clk1(c1), .Clk2(c2), .DataIn(in));
+
   initial begin
     c1 = 1'b1; c2 = 1'b0;
-	forever begin
-	  #2.5 c1 = ~c1; #2.5; 
-	  #2.5 c2 = ~c2; #2.5;
-	end
+    forever begin
+      #5;
+      c1 = ~c1;
+      c2 = ~c2;
+      /*
+      #2.5; c1 = ~c1; #2.5;
+      #2.5; c2 = ~c2; #2.5;
+      */
+    end
   end
-initial begin
-  $monitor("Current Instruc: %h, PC: %d, Addr: %h", 
-            UUT.instruction, UUT.PC, UUT.Addr);
-end
-initial begin
-  rst = 1'b1;
-  #10;
-  rst = 1'b0;
-  #10;
-  $strobe("Mem[0]: %h", mem.Memory[0]);
-  #1100;
-  $strobe("S0: %h S1: %h S7: %h", UUT.scalar.scalar[0], UUT.scalar.scalar[1], UUT.scalar.scalar[7]);
-  #10;
-  $finish;
-end  
+
+  initial begin
+    /*
+    $monitor("State: %b, Current Instruc: %h, PC: %d, Addr: %h, sWR_l: %b S0: %h S1: %h S7: %h",
+            UUT.state, UUT.instruction, UUT.PC, UUT.Addr, UUT.sWR_l,
+            UUT.scalar.scalar[0], UUT.scalar.scalar[1], UUT.scalar.scalar[7]);
+    */
+    /*
+    $monitor("%t: State %b, Inst: %h, sWR_l:, %b, Scalar 0: %h, sIn: %h, sAddr: %h", $time, UUT.state,
+          UUT.instruction, UUT.sWR_l, UUT.scalar.scalar[0], UUT.sIn, UUT.sAddr);
+    */
+  end
+
+  initial begin
+    rst = 1'b1;
+    #10;
+    rst = 1'b0;
+    #10;
+    $strobe("Mem[0]: %h", mem.Memory[0]);
+    #1100;
+    $strobe("S0: %h S1: %h S7: %h", UUT.scalar.scalar[0], UUT.scalar.scalar[1], UUT.scalar.scalar[7]);
+    #10;
+    $finish;
+  end
 endmodule
