baacb8e5a816b952ff6b5bb59874bf9481cdfa44
David <smartydude010@hotmail.com>
2014-03-28 12:29:35 -0500

    Overhauled vreg to remove latches

--- vReg.v
+++ vReg.v
@@ -6,103 +6,101 @@ module vReg(output reg [255:0] DataOut_p, output reg [255:0] DataOut2_p,
     input RD_p, input WR_p, input RD_s, input WR_s);
 
   reg [15:0] vector[7:0][15:0];
-
+  integer i;  //used in for loop
+  wire [3:0] cmd;
   reg [2:0] address, address2;
-  reg readp, writep, reads, writes;
+  parameter readp = 4'b1000, writep = 4'b0100, reads = 4'b0010, writes = 4'b0001;
+  reg prev_WR_s, prev_RD_s;
   reg [3:0] select;
-
+ 
+  assign cmd = {RD_p, WR_p, RD_s, WR_s}; 
+  
   always@(posedge Clk1) begin
-    readp <= RD_p;
-    writep <= WR_p;
-    reads <= RD_s;
-    writes <= WR_s;
+    prev_RD_s <= RD_s;
+    prev_WR_s <= WR_s;
 
-    if ((~reads && RD_s) || (~writes && WR_s))
+    if ((~prev_RD_s && RD_s) || (~prev_WR_s && WR_s))
       select <= 0;
     else if (reads || writes)
       select <= select + 1;
     else
       select <= select;
-  end
-
-  always@(posedge Clk2) begin
-    address <= Addr;
-    address2 <= Addr2;
-  end
-  
-  always@(readp, address, address2) begin
-    if (readp) begin
-      DataOut_p = {vector[address][15], vector[address][14], 
+	  
+	case (cmd)
+	  readp: begin
+	    DataOut_p <= {vector[address][15], vector[address][14], 
                    vector[address][13], vector[address][12], vector[address][11], 
                    vector[address][10], vector[address][9], vector[address][8], 
                    vector[address][7], vector[address][6], vector[address][5], 
                    vector[address][4], vector[address][3], vector[address][2], 
                    vector[address][1], vector[address][0]}; 
-      DataOut2_p = {vector[address2][15], vector[address2][14], 
+        DataOut2_p <= {vector[address2][15], vector[address2][14], 
                    vector[address2][13], vector[address2][12], vector[address2][11], 
                    vector[address2][10], vector[address2][9], vector[address2][8], 
                    vector[address2][7], vector[address2][6], vector[address2][5], 
                    vector[address2][4], vector[address2][3], vector[address2][2], 
                    vector[address2][1], vector[address2][0]}; 
-    end
-    else begin
-      DataOut_p = DataOut_p;
-      DataOut2_p = DataOut2_p;
-    end
+	    
+		for(i = 0; i < 16; i = i + 1) 
+		  vector[address][i] <= vector[address][i];
+	    
+	    DataOut_s <= DataOut_s;  
+        DataOut2_s <= DataOut2_s;
+	  end
+	  writep: begin
+	    vector[address][0] <= DataIn_p[15:0];
+        vector[address][1] <= DataIn_p[31:16];
+        vector[address][2] <= DataIn_p[47:32];
+        vector[address][3] <= DataIn_p[63:48];
+        vector[address][4] <= DataIn_p[79:64];
+        vector[address][5] <= DataIn_p[95:80];
+        vector[address][6] <= DataIn_p[111:96];
+        vector[address][7] <= DataIn_p[127:112];
+        vector[address][8] <= DataIn_p[143:128];
+        vector[address][9] <= DataIn_p[159:144];
+        vector[address][10] <= DataIn_p[175:160];
+        vector[address][11] <= DataIn_p[191:176];
+        vector[address][12] <= DataIn_p[207:192];
+        vector[address][13] <= DataIn_p[223:208];
+        vector[address][14] <= DataIn_p[239:224];
+        vector[address][15] <= DataIn_p[255:240];
+		
+		DataOut_p <= DataOut_p;
+        DataOut2_p <= DataOut2_p;
+		
+		DataOut_s <= DataOut_s;  
+        DataOut2_s <= DataOut2_s;
+	  end  
+	  reads: begin
+	    DataOut_s = vector[address][select];
+        DataOut2_s = vector[address2][select];
+	  end  
+	  writes: begin 
+	    vector[address][select] <= DataIn_s;
+		
+		DataOut_p <= DataOut_p;
+        DataOut2_p <= DataOut2_p;
+		
+		for(i = 0; i < 16; i = i + 1) 
+		  vector[address][i] <= vector[address][i];
+	  end
+	  default: begin
+	    DataOut_p <= DataOut_p;
+        DataOut2_p <= DataOut2_p;
+		
+		for(i = 0; i < 16; i = i + 1) 
+		  vector[address][i] <= vector[address][i];
+	    
+	    DataOut_s <= DataOut_s;  
+        DataOut2_s <= DataOut2_s;
+
+	  end
+	endcase
   end
-  
-  always@(writep, address) begin
-    if (writep) begin
-      vector[address][0] = DataIn_p[15:0];
-      vector[address][1] = DataIn_p[31:16];
-      vector[address][2] = DataIn_p[47:32];
-      vector[address][3] = DataIn_p[63:48];
-      vector[address][4] = DataIn_p[79:64];
-      vector[address][5] = DataIn_p[95:80];
-      vector[address][6] = DataIn_p[111:96];
-      vector[address][7] = DataIn_p[127:112];
-      vector[address][8] = DataIn_p[143:128];
-      vector[address][9] = DataIn_p[159:144];
-      vector[address][10] = DataIn_p[175:160];
-      vector[address][11] = DataIn_p[191:176];
-      vector[address][12] = DataIn_p[207:192];
-      vector[address][13] = DataIn_p[223:208];
-      vector[address][14] = DataIn_p[239:224];
-      vector[address][15] = DataIn_p[255:240];
-    end
-    else begin
-      vector[address][0] = vector[address][0]; 
-      vector[address][1] = vector[address][1]; 
-      vector[address][2] = vector[address][2]; 
-      vector[address][3] = vector[address][3]; 
-      vector[address][4] = vector[address][4]; 
-      vector[address][5] = vector[address][5]; 
-      vector[address][6] = vector[address][6]; 
-      vector[address][7] = vector[address][7]; 
-      vector[address][8] = vector[address][8]; 
-      vector[address][9] = vector[address][9]; 
-      vector[address][10] = vector[address][10]; 
-      vector[address][11] = vector[address][11]; 
-      vector[address][12] = vector[address][12]; 
-      vector[address][13] = vector[address][13]; 
-      vector[address][14] = vector[address][14]; 
-      vector[address][15] = vector[address][15]; 
-    end
-  end
-  
-  always@(select) begin
-    if (reads) begin
-      DataOut_s = vector[address][select];
-      DataOut2_s = vector[address2][select];
-    end
-    else begin
-      DataOut_s = DataOut_s;  
-      DataOut2_s = DataOut2_s;  
-    end
-    if (writes)
-      vector[address][select] = DataIn_s;
-    else
-      vector[address][select] = vector[address][select];
+
+  always@(posedge Clk2) begin
+    address <= Addr;
+    address2 <= Addr2;
   end
 
 endmodule
