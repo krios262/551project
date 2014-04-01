701900964d80cb69d7a93dc7fb147d69d6070eb8
Evan Thompson <evan.g.thompson@gmail.com>
2014-03-31 22:29:33 -0500

    Scalar ops and jump work post-synthesis

--- t_CVP14_synth.v
+++ t_CVP14_synth.v
@@ -10,7 +10,7 @@ module t_CVP14_synth();
     .DataOut(out), .Reset(rst), .Clk1(c1), .Clk2(c2), .DataIn(in));
 
   initial begin
-    c1 = 1'b1; c2 = 1'b0;
+    c1 = 1'b0; c2 = 1'b1;
     forever begin
       #5;
       c1 = ~c1;
@@ -22,7 +22,11 @@ module t_CVP14_synth();
     end
   end
 
+  initial $monitor ("Clk1: %b Clk2: %b State: %b PC: %h Addr: %h SetPC: %b DataIn: %h, updatePC: %b",
+    UUT.Clk1, UUT.Clk2, UUT.state, UUT.PC, UUT.Addr, UUT.setPC, UUT.DataIn, UUT.n299);
+
   initial begin
+    #2.5;
     rst = 1'b1;
     #10;
     rst = 1'b0;
